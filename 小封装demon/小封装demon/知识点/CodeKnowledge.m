27715ecbba040a08a4ecbb4f7024866c95410678
jinfeng.niu <jinfeng.niu@phicomm.com>
2018-06-01 13:58:25 +0800

    [Module] 小封装 [Bug] N/A [Title] EnumerateObjectsUsingBlock的使用

--- 小封装demon/小封装demon/知识点/CodeKnowledge.m
+++ 小封装demon/小封装demon/知识点/CodeKnowledge.m
@@ -48,4 +48,50 @@ extern uint64_t dispatch_benchmark(size_t count, void (^block)(void));
     NSLog(@"for { @autoreleasepool { } } = %llu ns", time2);
 }
 
+#pragma mark - EnumerateObjectsUsingBlock,enumerateKeysAndObjectsUsingBlock的使用
+
+- (void)test3{
+    NSDictionary * dic = [NSDictionary dictionaryWithObjectsAndKeys:@"obj1",@"key1",@"obj2",@"key2", nil];
+    [dic enumerateKeysAndObjectsUsingBlock:^(id key, id value, BOOL *stop) {
+        NSLog(@"value for key %@ is %@ ", key, value);
+        if ([@"key2" isEqualToString:key]) {
+            *stop = YES;
+        }
+    }];
+}
+
+- (void)test4{
+    NSArray *array = [[NSArray alloc]initWithObjects:@"a",@"b",@"c",@"d",@"e",@"f", nil];
+    
+    //遍历数组元素
+    [array enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSLog(@"obj=%@   idx=%ld",obj,idx);
+    }];
+    
+    //如果指定了NSEnumerationConcurrent顺序，那么底层通过GCD来处理并发执行事宜，具体实现可能会用到dispatch group。也就是说，这个会用多线程来并发实现，并不保证按照顺序执行
+    
+    //NSEnumerationReverse 倒序排列
+    [array enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(id obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSLog(@"idx=%ld, id=%@", idx, obj);
+        
+        //当需要结束循环的时候,调用stop,赋予YES
+        if (idx ==3) {
+            *stop = YES;
+        }
+        
+    }];
+    //NSIndexSet类代表一个不可变的独特的无符号整数的集合,称为索引,因为使用它们的方式。这个集合被称为索引集    唯一的，有序的，无符号整数的集合
+    [NSIndexSet indexSetWithIndex:1];//创建一个索引集合，根据索引值
+    [NSIndexSet indexSetWithIndexesInRange:NSMakeRange(0,8)];//创建一个索引集合，根据一个NSRange对象
+    
+    [array enumerateObjectsAtIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(0,3)] options:NSEnumerationReverse usingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSLog(@"\n\n\nidx=%ld, id=%@", idx, obj);
+    }];
+}
+/*
+for in、经典for循环和EnumerateObjectsUsingBlock 的比较
+对于集合中对象数很多的情况下，for in 的遍历速度非常之快，但小规模的遍历并不明显（还没普通for循环快）
+Value查询index的时候, 面对大量的数组推荐使用 enumerateObjectsWithOptions的并行方法.
+遍历字典类型的时候, 推荐使用enumerateKeysAndObjectsUsingBlock,block版本的字典遍历可以同时取key和value（forin只能取key再手动取value）
+*/
 @end
